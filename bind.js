3fe3f2e784ac9530d54b21450bbe7e976ee038f9
ghgfh <1@gmail.com>
2018-11-06 13:28:17 +0200

    bind file added

---
+++ bind.js
@@ -0,0 +1,9 @@
+let createTemplate=require("./MakeBoardsFromTemplate");
+var templateA = {name: 'Template A', id: '5bcc8ce2c3b57a2906f49329'}
+var templateB = {name: 'Template b', id: '5bc4b0223b430a85843102bc'}
+var templateC = {name: 'running board', id: '5bcdd134654a6d884f18819b'}
+
+createTemplate.getTemplate(templateC); 
+var a =4;
+
+
