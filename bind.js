e932a2c76e6bd15ee3c9db7a73e828caa1515c83
ghgfh <1@gmail.com>
2018-11-06 14:45:39 +0200

    some order added

--- bind.js
+++ bind.js
@@ -1,9 +1,12 @@
 let createTemplate=require("./MakeBoardsFromTemplate");
-var templateA = {name: 'Template A', id: '5bcc8ce2c3b57a2906f49329'}
-var templateB = {name: 'Template b', id: '5bc4b0223b430a85843102bc'}
-var templateC = {name: 'running board', id: '5bcdd134654a6d884f18819b'}
 
-// createTemplate.getTemplate(templateC); 
+
+function getTemplateNameAndID(){
+    return {name: 'running board', id: '5bcdd134654a6d884f18819b'}
+  
+}
+
+// createTemplate.getTemplate(getTemplateNameAndID()); 
 
 
 
