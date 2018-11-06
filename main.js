f810fd85b47b9075644672a42bdce3ef0e374135
ghgfh <1@gmail.com>
2018-11-06 16:25:24 +0200

    file name changed to main

---
+++ main.js
@@ -0,0 +1,14 @@
+let createTemplate=require("./MakeBoardsFromTemplate");
+
+
+function getTemplateNameAndID(){
+    return {name: 'running board', id: '5bcdd134654a6d884f18819b'}
+  
+}
+
+// createTemplate.getTemplate(getTemplateNameAndID()); 
+
+
+
+
+
