b38a2e07c08738217bfba583f348df8d7a4ebe6f
ghgfh <1@gmail.com>
2018-11-06 13:33:32 +0200

    deleting unrelavent code

--- bind.js
+++ bind.js
@@ -4,6 +4,6 @@ var templateB = {name: 'Template b', id: '5bc4b0223b430a85843102bc'}
 var templateC = {name: 'running board', id: '5bcdd134654a6d884f18819b'}
 
 createTemplate.getTemplate(templateC); 
-var a =4;
+
 
 
